50b5eb7ffacc59edb499f2392381a4c2e7cce899
Sam Moore <swmoore84@gmail.com>
2021-03-05 12:41:17 +0900

    add controller tests for store

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,11 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    # asserts for an element named a, in a nav element that has the class side_nav
+    #checks that 4 such elements exist
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 end
